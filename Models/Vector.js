62d04a6607a3eaa99291ab46ee0faf98d1a56415
Jake Pfaller <jpfaller@cern.ch>
2024-01-27 13:29:43 +0100

    Added vector and matrix classes

---
+++ Models/Vector.js
@@ -0,0 +1,12 @@
+/**
+ * Vector class, inherits from Array
+ * @constructor
+ * @param {number} args - Vector elements
+ */
+
+class Vector extends Array {
+  constructor(...args) {
+    super(...args);
+  }
+}
+
